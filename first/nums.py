0b3317e339e7782bd622bd8c407f9dc78ae947ea
CreeperBoom07 <nmartianoff07@yandex.ru>
2023-02-18 18:04:00 +0300

    Небольшие шалости

--- first/nums.py
+++ first/nums.py
@@ -2,10 +2,10 @@ def plus(a, b):
     return a + b
 
 def mul(a, b):
-    return a*b
+    return 2*a*b
 
 def minus(a, b):
-    return a - b
+    return 2+a - b
 
 if __name__ == '__main__':
     print(plus(2, 2))
